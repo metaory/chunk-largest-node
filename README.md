06271f39d84a858974af08ccfbf43cd85714f334
metaory <metaory@gmail.com>
2024-03-29 16:47:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+Chunk Largest Node
+==================
+
+# Chunk large nested datasets
+
+## While maintaining the original data structure
+
+TODO
+====
+- [ ] Publish
+- [ ] Add examples
+- [ ] Makefile
+
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,25 @@
+import { readFileSync } from "node:fs";
+
+
+
+function traverse_to_find_longest (data) {
+  const keys = Object.keys(data)
+}
+
+
+function traverse(data, candidate) {
+
+}
+
+
+function init(data) {
+  return data
+}
+
+
+const input = JSON.parse(readFileSync('./.dev/sample-3.json', { encoding: 'utf8' }))
+console.log(' [KEYS]:input::', Object.keys(input))
+
+console.log('  [------  >>>>  ---··· ')
+console.log(init(init))
+console.log('  ···---  <<<<  ------]')
